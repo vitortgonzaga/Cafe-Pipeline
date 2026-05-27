fe4f25556cb851f1403d72f2ce2df4315e270246
Zeca <zeca1609@gmail.com>
2026-05-27 18:45:05 -0300

    feat: add morgan for HTTP request logging and update dependencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import { healthRoutes } from "./routes/health.routes";
 import { itemRoutes } from "./routes/item.routes";
 import { errorHandler } from "./middlewares/error-handler";
@@ -8,6 +9,7 @@ import { AppError } from "./errors/app-error";
 export const app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(healthRoutes);
